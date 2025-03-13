ec6839ed74437a2ec66736c703fb204b030c5e27
padma <padma@MY-PC.lan>
2025-03-13 22:21:04 +0530

    java learnings hello world even or oadd  largest between three number  prime number  sum of teo numbers.

---
+++ java_learnings/src/java_learnings/Sum.java
@@ -0,0 +1,17 @@
+package java_learnings;
+
+import java.util.*;
+
+public class Sum {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter num1");
+		int num1 = sc.nextInt();
+		System.out.println("enter num1");
+		int num2 = sc.nextInt();
+		int num3 = num1+num2;
+		System.out.println("sum :" + num3);
+	
+		
+	}
+}
